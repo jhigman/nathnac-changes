69562a95914842d2b046bcd1465d51ebd7887a56
Julian Higman <jhigman@pobox.com>
2015-03-18 09:30:00 +0000

    Added script to commit changes with timestamp

---
+++ send.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+empty_files=`find . -size 0`
+
+if [ $empty_files ]; then
+	echo "Empty files exist - not committing changes"
+else
+	echo "Committing changes with timestamp $timestamp.."
+	timestamp=`date +"%Y-%m-%d %H:%M"`
+	git commit -am "$timestamp"
+	echo "Finished"
+fi
+
