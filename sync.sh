f817869a9d0db2578b9ca6d8e301830c57f887e9
Julian Higman <jhigman@pobox.com>
2015-07-24 12:43:20 +0100

    Moved scripts to separate project

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-while read line
-do
-    parts=($line)
-    echo "curl -s ${parts[0]} | html2text -o ${parts[1]}.txt"
-    curl -s ${parts[0]} | html2text -o ${parts[1]}.txt
-done < $1
-
